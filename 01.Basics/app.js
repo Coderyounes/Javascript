f59900160f969710e0cbfac01ed1139d4d5ea916
CoderYounes <younesanas2021@gmail.com>
2023-12-10 21:14:37 +0100

    some console log usage

--- 01.Basics/app.js
+++ 01.Basics/app.js
@@ -1,1 +1,10 @@
-console.log("Hello Younes");
+/* Multi Line
+Comment */
+
+// one line comment
+
+console.log("Hello JS");
+console.warn("This is Warning");
+console.log({isStudent: "True"});
+console.table({Fname: "Younes", Age: 27, isStudent: "True"});
+
