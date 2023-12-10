6de6f8c954daa885e80ada5d989faf51485b3967
CoderYounes <younesanas2021@gmail.com>
2023-12-10 22:38:00 +0100

    comparaison Operators

---
+++ 02.primitives/04.comparison Operator/app.js
@@ -0,0 +1,14 @@
+// Comparison Operatos
+// -> Relationnal Operators
+
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 < 10);
+console.log(10 <= 10);
+
+// -> Equality Operators
+console.log(10 == 10); // check equalty
+console.log(10 != 10); // check not equality
+console.log(10 === 10); // Strict equlaty (type + value);
+console.log(10 !== 10); // strict not equality (type + value);
+
