eb71036d4c9522798102e20acda902cdc91ceb21
CoderYounes <younesanas2021@gmail.com>
2023-12-10 22:24:08 +0100

    Some Boolean practice & falsy values

---
+++ 02.primitives/03.Booleans/app.js
@@ -0,0 +1,22 @@
+// Boolean  -> True or False
+
+let isStudent = true;
+console.log(isStudent);
+
+let isMale = false;
+console.log(isMale);
+
+/* in JavasScript we have falsy Values :
+ -> NaN
+ -> false
+ -> null
+ -> undefined
+ -> 0
+ -> -0
+ -> '', "", backticks, ()
+ */
+
+ let number = undefined;
+ console.log(number);
+ let number2 = null;
+ console.log(number2);
