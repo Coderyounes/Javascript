475797e82d32755db17f18b1f18a438f25295bf0
CoderYounes <younesanas2021@gmail.com>
2023-12-10 21:49:35 +0100

    Some Arth Operation

---
+++ 02.primitives/02.Numbers/app.js
@@ -0,0 +1,37 @@
+// Declare & Assign integer or Float to a variable
+let num = 25;
+console.log(num);
+let negNum = -25;
+console.log(negNum);
+let FloaNum = 55.80;
+console.log(FloaNum);
+
+// Get the type of the variable
+console.log(typeof num);
+console.log(typeof "Hello");
+console.log(typeof FloaNum);
+
+
+// Basic Math
+console.log(5 + 5); // Add
+console.log(5 - 5); // Substraction
+console.log(5 * 5); // Multiplication
+console.log(5 / 5); // Division
+console.log(5 % 5); // Modulo
+console.log(5 ** 5); // Power
+
+console.clear();
+// Increment Variable
+let content = 0;
+content = content + 1;
+console.log(content);
+// other way
+content = 0;
+content += 1;
+console.log(content);
+// other way
+content = 0;
+content++;
+console.log(content);
+
+// same Method can be used with Other arthimitic Operations
