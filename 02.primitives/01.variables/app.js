c1267e00bb1d8b4a1d94dd93a97260be2f6b47e4
CoderYounes <younesanas2021@gmail.com>
2023-12-10 21:36:06 +0100

    Declaring & Assign Variables

---
+++ 02.primitives/01.variables/app.js
@@ -0,0 +1,18 @@
+// variables there some name that are reserved
+// declating let for the best Practice
+
+// declaring & assigning a String
+let name; 
+name = "Javascript"
+console.log(name);
+// an integer
+let number = 5;
+console.log(number);
+// Perform sum Operation
+let num1 = 5, num2 = 6;
+console.log(num1 + num2);
+//const is an unchangable
+const BornIn = 1996;
+console.log(BornIn);
+
+// Warning : let var = 'value'  var is a reserved Word
